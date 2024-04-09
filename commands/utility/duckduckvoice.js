c29e865d2527b2917fdfeb797715a0479f4161ec
Cyberwizard-v <veke2003@Outlook.com>
2024-04-09 16:58:39 +0200

    Duckbot functionality | Join voice and play quack!

---
+++ commands/utility/duckduckvoice.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-inline-comments */
+const { SlashCommandBuilder } = require('discord.js');
+const { joinVoiceChannel, getVoiceConnection, createAudioPlayer, createAudioResource, StreamType } = require('@discordjs/voice');
+const { createReadStream } = require('fs');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('duckduckvoice')
+		.setDescription('Joins a voice channel and makes a quack sound!'),
+	async execute(interaction) {
+		if (!interaction.guild) {
+			await interaction.reply('This command must be used in a guild.');
+			return;
+		}
+
+		const guildId = interaction.guildId;
+		const member = interaction.member;
+		const voiceChannel = member.voice.channel;
+
+		if (!voiceChannel) {
+			await interaction.reply('You need to be in a voice channel to use this command.');
+			return;
+		}
+
+		const channelId = voiceChannel.id; // Retrieve the channel ID of the voice channel
+
+		const voiceAdapterCreator = interaction.guild.voiceAdapterCreator;
+
+		if (!voiceAdapterCreator) {
+			await interaction.reply('The voice adapter creator is not available in this guild.');
+			return;
+		}
+
+		console.log(`Joining voice channel: ${voiceChannel.name} (${channelId})`);
+
+		const connection = joinVoiceChannel({
+			channelId: channelId,
+			guildId: guildId,
+			adapterCreator: voiceAdapterCreator,
+		});
+
+		const player = createAudioPlayer();
+
+		const audioResource = createAudioResource(createReadStream('quack.wav'), {
+			inputType: StreamType.Arbitrary,
+		});
+
+		connection.subscribe(player);
+
+		await new Promise(resolve => setTimeout(resolve, 2000));
+
+		player.play(audioResource);
+
+		setTimeout(() => {
+			const test = getVoiceConnection(guildId);
+			if (test) {
+				test.destroy();
+				console.log('Left voice channel.');
+			}
+		}, 10000); // 10 seconds
+
+		await interaction.reply('QUACKKK!!!!');
+	},
+};
--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildVoiceStates,
+	],
+});
 
 // When the client is ready, run this code (only once).
 client.once(Events.ClientReady, readyClient => {
