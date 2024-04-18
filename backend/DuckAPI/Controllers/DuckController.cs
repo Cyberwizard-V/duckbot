39734b3604ba41128da6720a4e563944fe034532
Cyberwizard-v <veke2003@Outlook.com>
2024-04-18 23:52:48 +0200

    Duck Controller | HttpGET

---
+++ backend/DuckAPI/Controllers/DuckController.cs
@@ -0,0 +1,29 @@
+﻿using DuckAPI.Interfaces;
+using DuckAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DuckAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DuckController : Controller
+    {
+        private readonly IDuckRepository _duckRepository;
+        public DuckController(IDuckRepository duckRepository)
+        {
+            _duckRepository = duckRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Duck>))]
+        public IActionResult GetDucks()
+        {
+            var ducks = _duckRepository.GetDucks();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(ducks);
+        }
+    }
+}
