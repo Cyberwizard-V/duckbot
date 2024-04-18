068a0a0ea54a1360e08f042fa9282cd9f5d49c57
Cyberwizard-v <veke2003@Outlook.com>
2024-04-18 23:52:19 +0200

    Duck repository

---
+++ backend/DuckAPI/Repository/DuckRepository.cs
@@ -0,0 +1,20 @@
+﻿using DuckAPI.Data;
+using DuckAPI.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DuckAPI.Repository
+{
+    public class DuckRepository : IDuckRepository
+    {
+        private readonly DataContext _context;
+        public DuckRepository(DataContext context) 
+        {
+            _context = context;
+        }
+
+        public ICollection<Models.Duck> GetDucks()
+        {
+            return _context.Ducks.ToList();
+        }
+    }
+}
