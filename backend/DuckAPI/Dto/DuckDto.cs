5180aa98cba9d4234cd4e5557455c52b80ba49f2
BMODEV <v.stam2003@Outlook.com>
2024-04-25 00:21:25 +0200

    DTO and mapper

---
+++ backend/DuckAPI/Dto/DuckDto.cs
@@ -0,0 +1,11 @@
+﻿using DuckAPI.Models;
+
+namespace DuckAPI.Dto
+{
+    public class DuckDto
+    {
+        public string DuckName { get; set; }
+        public string DuckColor { get; set; }
+        public string DuckGender { get; set; }
+    }
+}
---
+++ backend/DuckAPI/Mapper/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DuckAPI.Dto;
+using DuckAPI.Models;
+
+namespace DuckAPI.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Duck, DuckDto>();
+
+        }
+    }
+}
