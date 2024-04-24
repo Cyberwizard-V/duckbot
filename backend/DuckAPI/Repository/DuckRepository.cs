4d43910eba5db3e61ae0cf908e9bbcf42f9b850f
BMODEV <v.stam2003@Outlook.com>
2024-04-25 00:21:50 +0200

    Repository update

--- backend/DuckAPI/Repository/DuckRepository.cs
+++ backend/DuckAPI/Repository/DuckRepository.cs
@@ -1,5 +1,6 @@
 ﻿using DuckAPI.Data;
 using DuckAPI.Interfaces;
+using DuckAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace DuckAPI.Repository
@@ -16,5 +17,21 @@ namespace DuckAPI.Repository
         {
             return _context.Ducks.ToList();
         }
+
+        public bool DuckExists(int id)
+        {
+            return _context.Ducks.Any(p => p.DuckID == id);
+        }
+
+        public Duck getDuckByID(int id)
+        {
+            return _context.Ducks.Where(p => p.DuckID == id).FirstOrDefault();
+        }
+
+        public Duck getDuckByName(string name)
+        {
+            return _context.Ducks.Where(p => p.DuckName == name).FirstOrDefault();
+        }
+
     }
 }
