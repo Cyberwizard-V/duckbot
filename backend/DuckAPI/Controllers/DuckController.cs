91bb15cb7dbad1b7c699dfe9bf01b09f37660e83
BMODEV <v.stam2003@Outlook.com>
2024-04-25 00:22:30 +0200

    GET request | get duck by ID

--- backend/DuckAPI/Controllers/DuckController.cs
+++ backend/DuckAPI/Controllers/DuckController.cs
@@ -1,6 +1,9 @@
-﻿using DuckAPI.Interfaces;
+﻿using AutoMapper;
+using DuckAPI.Dto;
+using DuckAPI.Interfaces;
 using DuckAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace DuckAPI.Controllers
 {
@@ -9,21 +12,40 @@ namespace DuckAPI.Controllers
     public class DuckController : Controller
     {
         private readonly IDuckRepository _duckRepository;
-        public DuckController(IDuckRepository duckRepository)
+        private readonly IMapper _mapper;
+        public DuckController(IDuckRepository duckRepository, IMapper mapper)
         {
             _duckRepository = duckRepository;
+            _mapper = mapper;
         }
 
         [HttpGet]
         [ProducesResponseType(200, Type = typeof(IEnumerable<Duck>))]
         public IActionResult GetDucks()
         {
-            var ducks = _duckRepository.GetDucks();
+            var ducks = _mapper.Map<List<DuckDto>>(_duckRepository.GetDucks());
 
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
             return Ok(ducks);
+        }
+
+        [HttpGet("{DuckID}")]
+        [ProducesResponseType(200, Type = typeof(Duck))]
+        [ProducesResponseType(400)]
+        public IActionResult GetDuckByID(int DuckID)
+        {
+
+            if (!_duckRepository.DuckExists(DuckID))
+                return NotFound();
+            
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var duck = _mapper.Map<DuckDto>(_duckRepository.getDuckByID(DuckID));
+
+            return Ok(duck);
         }
     }
 }
